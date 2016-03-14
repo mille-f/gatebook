15dc9f325743bd99f0db87de98ca93610d7c4864
mille-f <f11235tetra@gmail.com>
2016-03-14 09:13:24 +0900

    [add] update method

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/notes' => 'notes#index'
   get '/notes/:id' => 'notes#show', as: 'note'
   get '/notes/:id/edit' => 'notes#edit', as: 'edit_note'
+  patch '/notes/:id' => 'notes#update', as: 'update_note'
   root 'home#top'
   get '/about' => 'home#about'
 end
